a92712291abdc731e18b9893763d584f89e1964e
Ege <ege.secr3t@gmail.com>
2021-01-24 17:36:23 +0300

    DamaTahtası

---
+++ deneme/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deneme
+{
+    class Class1
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Sehri { get; set; }
+    }
+}
---
+++ deneme/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 musteri1 = new Class1();
+            musteri1.Adi = "Ege";
+            musteri1.Soyadi = "Yurt";
+            musteri1.Id = 3;
+            musteri1.Sehri = "Bursa";
+
+            Console.WriteLine(musteri1.Adi +" " + musteri1.Soyadi + ":"  + musteri1.Sehri);
+
+        }
+    }
+}
