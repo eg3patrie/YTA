8c2ce3dd215da5382bc499e568cc0e9f3ea28bec
Ege <ege.secr3t@gmail.com>
2021-04-25 21:52:21 +0300

    25.04.2021

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Maraz Ali";
+            int yas = 31;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "c#";
+            kurs1.KursunEgitmeni = "Maraz Ali";
+            kurs1.İzlenmeOrani = 70;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "javascript";
+            kurs2.KursunEgitmeni = "Ege Yurt";
+            kurs2.İzlenmeOrani = 100;
+           
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "python";
+            kurs3.KursunEgitmeni = "Ata Yurt";
+            kurs3.İzlenmeOrani = 50;
+
+            
+
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi+":"+kurs.KursunEgitmeni);
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string KursunEgitmeni { get; set; }
+        public int İzlenmeOrani { get; set; }   
+    }
+}
---
+++ hatirlama.sc/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace hatirlama.sc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yas = 50;
+            Console.WriteLine(yas);
+            string adi = "Ege";
+            Console.WriteLine(adi);
+            double dolarTL = 8.23;
+            Console.WriteLine(dolarTL);
+            bool giris = (false);
+
+            if (giris == false)
+            {
+                Console.WriteLine("Giriş Butonu");
+            }
+            else if (giris==true)
+            {
+                Console.WriteLine("Giriş Başarılı");
+            }
+            Console.WriteLine("-------------------------");
+
+            string[] kullanici = new string[] {"Ege","Ata","Bülent","Nurgül" };
+
+            for (int i = 0; i < kullanici.Length; i++)
+            {
+                Console.WriteLine(kullanici[i]);
+            }
+            Console.WriteLine("----------------");
+
+            foreach (string kullanmak in kullanici)
+            {
+                Console.WriteLine(kullanmak);
+
+            }
+
+
+
+        }
+    }
+}
